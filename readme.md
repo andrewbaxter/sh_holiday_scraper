3f5aba1da551197a57b99091b96e88c09eef6cd4
Rendaw <rendaw@zarbosoft.com>
2015-01-12 23:14:31 +0900

    Initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Usage
+
+## http://www.timeanddate.com/
+
+Note: Country names are typically lower case, and either hyphenated or acronymed if multiple words.  'the' and 'republic' and possibly other common words are discarded.
+
+### Usage
+
+```bash
+scrapy timeanddate_com -a country=COUNTRY
+```
+
---
+++ sh_holiday_scraper/spiders/timeanddate_com.py
@@ -0,0 +1,33 @@
+import scrapy
+import scrapy.http
+import dateutil.parser
+
+class TimeAndDate_Com(scrapy.Spider):
+    name = "timeanddate_com"
+
+    def __init__(self, country):
+        super(TimeAndDate_Com, self).__init__()
+        self.country = country
+
+    def start_requests(self):
+        yield scrapy.http.Request('http://www.timeanddate.com/holidays/{}/'.format(self.country))
+
+    def parse(self, response):
+        days = []
+        for row in response.xpath('//table[contains(@class,"tb-cl")]/tbody/tr'):
+            columns = [u''.join(column.xpath('.//text()').extract()) for column in row.xpath('./td|./th')]
+            holiday_type = columns[-1]
+            if holiday_type not in ['National holiday', 'Bank holiday']:
+                continue
+            days.append({
+                'date': repr(str(dateutil.parser.parse(columns[0]).date())),
+                'title': columns[2],
+            })
+        print(
+            u'Holidays are:\n    {{\n{}    }}'.format(u''.join([
+                u'        {},  # {}\n'.format(
+                    day['date'], 
+                    day['title'],
+                ) for day in days
+            ]))
+        )
